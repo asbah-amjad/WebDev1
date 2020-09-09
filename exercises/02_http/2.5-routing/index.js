e62956f40c01bfcadd2f9dbabfa168736bac6ff3
khasus <asbahamjad.usmani@tuni.fi>
2020-09-09 19:02:59 +0300

    First try

---
+++ exercises/02_http/2.5-routing/index.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http
+  .createServer(function (req, res) {
+    var url = req.url;
+
+    if (url === " /classical") {
+      fs.readFile("homer.html", "utf8", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (url === "/modern") {
+      fs.readFile("bradbury.html", "utf8", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (url === "/") {
+      fs.readFile("index.html", "utf8", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      res.statusCode = 404;
+      res.statusMessage = "Requested content not found";
+      res.end();
+    }
+  })
+  .listen(3000);
