e4454b7e97f24045a37a6436c0d40b923e1088b3
khasus <asbahamjad.usmani@tuni.fi>
2020-09-09 19:08:12 +0300

    modified

--- exercises/02_http/2.5-routing/index.js
+++ exercises/02_http/2.5-routing/index.js
@@ -5,7 +5,7 @@ var server = http
   .createServer(function (req, res) {
     var url = req.url;
 
-    if (url === " /classical") {
+    if (url === "/classical") {
       fs.readFile("homer.html", "utf8", function (err, data) {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
