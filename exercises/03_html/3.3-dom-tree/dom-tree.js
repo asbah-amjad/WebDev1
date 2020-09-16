024e24afd7ab08606c82316993701d9fbe365d2e
khasus <asbahamjad.usmani@tuni.fi>
2020-09-16 21:16:32 +0300

    try

--- exercises/03_html/3.3-dom-tree/dom-tree.js
+++ exercises/03_html/3.3-dom-tree/dom-tree.js
@@ -5,5 +5,17 @@ var li = document.createElement("li");
 var a = document.createElement("a");
 li.appendChild(a);
 a.href = "http://localhost:3000/";
-a.textContent = "Localhost inside";
+a.text = "Localhost inside";
 element1.appendChild(li);
+
+var li1 = document.createElement("li");
+//li1.innerHTML = "Item 0";
+const newContent = document.createTextNode("Item 0");
+li1.appendChild(newContent);
+document.getElementById("ordered")[0] = li1;
+
+var element2 = document.getElementById("todo");
+element2.classList.remove("navi");
+
+var child = element2.children;
+element2.removeChild(child[1]);
