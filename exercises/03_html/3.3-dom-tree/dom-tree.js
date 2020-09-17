1eaa3c188706a7cf6b40eb326b76f384d8ec98a3
khasus <asbahamjad.usmani@tuni.fi>
2020-09-17 14:44:40 +0300

    DOM

--- exercises/03_html/3.3-dom-tree/dom-tree.js
+++ exercises/03_html/3.3-dom-tree/dom-tree.js
@@ -1,18 +1,19 @@
-var element1 = document.getElementsByClassName("navi");
+//var element1 = document.getElementsByClassName("navi");
+var element1 = document.querySelector("nav > ul");
 element1.classList.add("list");
 
 var li = document.createElement("li");
 var a = document.createElement("a");
 a.href = "http://localhost:3000/";
-a.text = "Localhost inside";
+a.textContent = "Localhost";
 li.appendChild(a);
 element1.appendChild(li);
 
 var li1 = document.createElement("li");
 //li1.innerHTML = "Item 0";
-const newContent = document.createTextNode("Item 0");
-li1.appendChild(newContent);
-document.getElementById("ordered")[0] = li1;
+li1.textContent = "Item 0";
+var ol = document.querySelector("ol");
+ol.insertBefore(li1, ol.childNodes[0]);
 
 var element2 = document.getElementById("todo");
 element2.classList.remove("navi");
