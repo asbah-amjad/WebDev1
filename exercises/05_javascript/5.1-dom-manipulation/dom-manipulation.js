30cf9c27657779c85d22e72e486a2806967d2bcb
khasus <asbahamjad.usmani@tuni.fi>
2020-09-29 19:43:07 +0300

    Done

--- exercises/05_javascript/5.1-dom-manipulation/dom-manipulation.js
+++ exercises/05_javascript/5.1-dom-manipulation/dom-manipulation.js
@@ -6,22 +6,40 @@
  */
 function sortTableByColumn(col, table) {
   // TODO: Implement this function
-  var i, switching;
-  var rows = table.rows,
-    rlen = rows.length;
-  var arr = new Array();
+  var rows, switching, i, x, y, shouldSwitch;
 
-  for (i = 1; i < rlen - 1; i++) {
-    //cells = rows[i].cells;
-    // clen = cells.length;
-    arr[i] = new Array();
-  }
-  arr.sort(function (a, b) {
-    if (a.localeCompare(b) > 0) switching = true;
-  });
-  for (i = 1; i < rlen - 1; i++) {
-    if (switching) {
+  switching = true;
+
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < rows.length - 1; i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[col];
+      y = rows[i + 1].getElementsByTagName("TD")[col];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        // If so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
     }
   }
 }
