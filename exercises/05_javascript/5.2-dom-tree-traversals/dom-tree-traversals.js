ddd822f4ce487b81ca65b8a355fc5fdc4e293c81
khasus <asbahamjad.usmani@tuni.fi>
2020-09-30 14:37:46 +0300

    Li traversal

---
+++ exercises/05_javascript/5.2-dom-tree-traversals/dom-tree-traversals.js
@@ -0,0 +1,8 @@
+//loop over li
+for (let li of document.querySelectorAll("li")) {
+  //get the descendants count
+
+  let count = li.getElementsByTagName("li").length;
+  li.innerHTML += count;
+}
+//document.getElementsByTagName("li").childElementCount
