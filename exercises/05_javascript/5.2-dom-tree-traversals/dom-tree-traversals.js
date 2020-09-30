13685b3dbac03e45b33019b2c12a06ed6b587945
khasus <asbahamjad.usmani@tuni.fi>
2020-09-30 15:50:36 +0300

    problem in counting

--- exercises/05_javascript/5.2-dom-tree-traversals/dom-tree-traversals.js
+++ exercises/05_javascript/5.2-dom-tree-traversals/dom-tree-traversals.js
@@ -1,8 +1,11 @@
 //loop over li
+
 for (let li of document.querySelectorAll("li")) {
   //get the descendants count
 
   let count = li.getElementsByTagName("li").length;
   li.innerHTML += "(" + count + ")";
+  //li.firstElementChild.innerHTML += "(" + count + ")";
 }
+
 //document.getElementsByTagName("li").childElementCount
