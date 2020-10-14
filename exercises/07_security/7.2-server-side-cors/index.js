8275615443ba0ed50d79f6b16d6a3a04ebac9fa6
khasus <asbahamjad.usmani@tuni.fi>
2020-10-14 14:36:35 +0300

    testing

--- exercises/07_security/7.2-server-side-cors/index.js
+++ exercises/07_security/7.2-server-side-cors/index.js
@@ -3,18 +3,47 @@ const port = 3000;
 
 http.createServer((req, res) => {
     // Remove the line 'res.end();' below when you start your own development
-    res.end();
-    const headers = {
-        /** TODO: add the required CORS headers */
-    };
-
+   // res.end();
+   
+    //const headers = {
+        /** TODO: add the required CORS headers */ 
+        
+    //};
+    
+    //if origin header is not set in req
+    if(!req.headers['origin']){
+      res.statusCode = 400;
+      res.statusMessage = "Origin header not in the request";
+      return res.end();
+    }
     // TODO: handle HEAD HTTP method, 
     // remember to add CORS headers to response
-
+    if(req.method === "HEAD"){
+        res.statusCode = 200;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods","HEAD, GET, POST");
+        res.setHeader("Access-Control-Max-Age", "7200");
+        return res.end();
+    }
     // TODO: handle GET and POST HTTP methods, 
     // remember to add CORS headers to response
-
+    else if(req.method === "GET" || req.method === "POST"){
+        res.statusCode = 200;
+        res.statusMessage = "I was requested using CORS!";
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods","HEAD, GET, POST");
+        res.setHeader("Access-Control-Max-Age", "7200");
+        return res.end();
+    }
     // TODO: handle HTTP methods that are not allowed, 
     // remember to add CORS headers to response
+    else{
+        res.statusCode = 405;
+        res.statusMessage = "Request used a HTTP method which is not allowed.";
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods","HEAD, GET, POST");
+        res.setHeader("Access-Control-Max-Age", "7200");
+        return res.end();
+    }
 
 }).listen(port);
