2f4d3ba767ecbd05f5d22894936c1cdc13a5c6cd
khasus <asbahamjad.usmani@tuni.fi>
2020-10-14 16:24:52 +0300

    CORS header outside if statements

--- exercises/07_security/7.2-server-side-cors/index.js
+++ exercises/07_security/7.2-server-side-cors/index.js
@@ -13,24 +13,23 @@ http.createServer((req, res) => {
     //if origin header is not set in req
     if(!req.headers['origin']){
       res.writeHead(400);
-     // res.statusMessage = "Origin header not in the request";
-      res.end("Origin header not in the request");
+      res.write("Origin header not in the request");
+      res.end();
     }
-    // TODO: handle HEAD HTTP method, 
-    // remember to add CORS headers to response
-    if(req.method === "HEAD"){
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods","HEAD, GET, POST");
         res.setHeader("Access-Control-Max-Age", "7200");
+    // TODO: handle HEAD HTTP method, 
+    // remember to add CORS headers to response
+    if(req.method === "HEAD"){
+        
         res.writeHead(200);
         res.end();
     }
     // TODO: handle GET and POST HTTP methods, 
     // remember to add CORS headers to response
     else if(req.method === "GET" || req.method === "POST"){
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods","HEAD, GET, POST");
-        res.setHeader("Access-Control-Max-Age", "7200");
+        
         res.writeHead(200);
         res.write("I was requested using CORS!");
         res.end();
@@ -38,9 +37,7 @@ http.createServer((req, res) => {
     // TODO: handle HTTP methods that are not allowed, 
     // remember to add CORS headers to response
     else{
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods","HEAD, GET, POST");
-        res.setHeader("Access-Control-Max-Age", "7200");
+        
         res.writeHead(405);
         res.write("Request used a HTTP method which is not allowed.");
         res.end();
