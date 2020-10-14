faf37519cf8c1e25ed2bea48928b4d2fa6e2c3d6
khasus <asbahamjad.usmani@tuni.fi>
2020-10-14 16:30:44 +0300

    return

--- exercises/07_security/7.2-server-side-cors/index.js
+++ exercises/07_security/7.2-server-side-cors/index.js
@@ -15,6 +15,7 @@ http.createServer((req, res) => {
       res.writeHead(400);
       res.write("Origin header not in the request");
       res.end();
+      return;
     }
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods","HEAD, GET, POST");
@@ -25,6 +26,7 @@ http.createServer((req, res) => {
         
         res.writeHead(200);
         res.end();
+        return;
     }
     // TODO: handle GET and POST HTTP methods, 
     // remember to add CORS headers to response
@@ -33,6 +35,7 @@ http.createServer((req, res) => {
         res.writeHead(200);
         res.write("I was requested using CORS!");
         res.end();
+        return;
     }
     // TODO: handle HTTP methods that are not allowed, 
     // remember to add CORS headers to response
@@ -41,6 +44,7 @@ http.createServer((req, res) => {
         res.writeHead(405);
         res.write("Request used a HTTP method which is not allowed.");
         res.end();
+        return;
     }
 
 }).listen(port);
